e1e5a2c54c5d08525733246e1dd4447903de546b
PandaPC <jose.fornes.jimenez@gmail.com>
2025-06-03 11:54:45 +0200

    Incluyendo carpeta .pio (no recomendado)

---
+++ .pio/libdeps/esp32-c3-devkitm-1/TFT_eSPI/User_Setup.h
@@ -0,0 +1,13 @@
+#define GC9A01_DRIVER
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 240
+
+#define TFT_CS   2
+#define TFT_DC   3
+#define TFT_RST  1
+
+#define TFT_MOSI 10
+#define TFT_SCLK 8
+
+#define LOAD_GLCD
+#define SPI_FREQUENCY  25000000
